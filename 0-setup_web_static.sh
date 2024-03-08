c469c1441d1dcb5d3859b894d4c5687528692798
ade-mic <ademic.aina@gmail.com>
2024-03-08 19:47:53 +0100

    new 0-setup..

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,7 +2,7 @@
 # A Bash script that sets up your web servers for the deployment of web_static
 
 # update the apt install
-sudo apt-get update
+apt-get update
 
 # Install nginx
 if [ "$(command -v nginx)" ]; then
@@ -11,13 +11,13 @@ else
     sudo apt-get install -y nginx
 fi
 
-sudo mkdir -p /data/;
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+mkdir -p /data/;
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
 # Create a fake HTML file /data/web_static/releases/test/index.html
-sudo tee /data/web_static/releases/test/index.html <<EOF
+tee /data/web_static/releases/test/index.html <<EOF
 <html>
   <head>
   </head>
@@ -28,15 +28,15 @@ sudo tee /data/web_static/releases/test/index.html <<EOF
 EOF
 
 # a symbolic link to  /data/web_static/current
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Give ownership of the /data/ folder to the ubuntu user AND group
-sudo chown -R ubuntu:ubuntu /data/
+chown -R ubuntu:ubuntu /data/
 # Update the Nginx configuration
-sudo tee /etc/nginx/sites-available/default <<EOF
+tee /etc/nginx/sites-available/default <<EOF
 server {
        location /hbnb_static/ {
                 alias /data/web_static/current/;
         }
 }
 EOF
-sudo nginx -s reload
+nginx -s reload
