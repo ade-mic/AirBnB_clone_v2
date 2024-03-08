c6b033b5a9702546e238e0fa88d64ffa7fdb1024
ade-mic <ademic.aina@gmail.com>
2024-03-08 22:00:52 +0100

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,44 +1,28 @@
 #!/usr/bin/python3
 """
-a Fabric script that generates a .tgz archive from the contents of the
-web_static folder of your AirBnB Clone repo, using the function
-do_pack
+Fabric script that generates a .tgz archive from the contents
+of the web_static folder of your AirBnB Clone repo
 """
-from fabric.api import *
-from datetime import datetime
 
+from fabric.api import local
+from datetime import datetime
+import os
 
 def do_pack():
-    """
-    Prototype: def do_pack():
-    All files in the folder web_static must be added to the final
-    archive
-    All archives must be stored in the folder versions
-    (your function should create this folder if it doesn’t exist)
-    The name of the archive created must be
-    web_static_<year><month><day><hour><minute><second>.tgz
-    The function do_pack must return the archive path if the
-    archive has been correctly generated.
-    Otherwise, it should return None
-    """
-    # Get the current date and time
-    now = datetime.now()
-    filename = "web_static_{}_{}_{}_{}_{}.tgz".format(now.year,
-                                                      now.month,
-                                                      now.day,
-                                                      now.hour,
-                                                      now.minute,
-                                                      now.second)
-    local("mkdir -p versions")
-    # capture the output
-    result = local("tar -cfvf versions/{} web_static".format(filename))
+    """Creates a .tgz archive from web_static folder."""
+    try:
+        if not os.path.exists("versions"):
+            os.makedirs("versions")
 
-    # check if the command failed
-    if result.failed:
+        now = datetime.now()
+        timestamp = now.strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(timestamp)
+
+        local("tar -cvzf {} web_static".format(archive_path))
+
+        if os.path.exists(archive_path):
+            return archive_path
+        else:
+            return None
+    except Exception as e:
         return None
-    else:
-        return "versions/{}".format(filename)
-
-
-if __name__ == "__main__":
-    do_pack()
