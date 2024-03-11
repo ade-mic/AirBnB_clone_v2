faf4f391c9a0460890f8bb573919e909d0142369
ade-mic <ademic.aina@gmail.com>
2024-03-11 07:09:41 +0100

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a .tgz archive from the contents
-of the web_static folder of your AirBnB Clone repo
+Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy
 """
 
 from fabric.api import *
@@ -12,7 +11,28 @@ env.hosts = ["54.85.90.192", "54.237.14.81"]
 env.username = "ubuntu"
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web servers."""
+    """
+    Prototype: def do_deploy(archive_path):
+    Returns False if the file at the path archive_path doesn’t exist
+    The script should take the following steps:
+    Upload the archive to the /tmp/ directory of the web server
+    Uncompress the archive to the folder
+    /data/web_static/releases/<archive filename without extension>
+    on the web server
+    Delete the archive from the web server
+    Delete the symbolic link /data/web_static/current
+    from the web server
+    Create a new the symbolic link /data/web_static/current
+    on the web server, linked to the new version of your code
+    (/data/web_static/releases/<archive filename without extension>)
+    All remote commands must be executed on your both web servers
+    (using env.hosts = ['<IP web-01>', 'IP web-02']
+    variable in your script)
+    Returns True if all operations have been done correctly,
+    otherwise returns False
+    You must use this script to deploy it on your servers:
+    xx-web-01 and xx-web-02
+    """
     if  not os.path.exists(archive_path):
         return False
     try:
