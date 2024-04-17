4e6d34a810b3c15ba5c93fd81f23ce884963e9a7
ade-mic <ademic.aina@gmail.com>
2024-04-17 10:58:51 +0100

    state

--- models/state.py
+++ models/state.py
@@ -8,18 +8,21 @@ import models
 from models.base_model import BaseModel, Base
 from models.city import City
 
+
 class State(BaseModel, Base):
     """Representation of state """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
     if models.storage_t == "db":
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
     else:
         @property
         def cities(self):
             """getter for list of city instances related to the state"""
-            return [city for city in models.storage.all(City).values() if city.state_id == self.id]
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
 
     def __init__(self, *args, **kwargs):
         """initializes state"""
