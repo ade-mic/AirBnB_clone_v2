5fa709d5fe2115f48f61e5316a59d82db990099d
ade-mic <ademic.aina@gmail.com>
2024-04-17 11:57:43 +0100

    states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template
 from models import storage
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-@app.route('/states_list')
+
+@app.route('/states_list', strict_slashes=False)
 def state_list():
     """ Handles the state route to display a HTML page"""
     states = storage.all("State").values()
