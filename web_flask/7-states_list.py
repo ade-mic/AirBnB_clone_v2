84944c5d297b5bd2c72de5007d7cff310b895ae1
ade-mic <ademic.aina@gmail.com>
2024-04-17 12:23:35 +0100

    storage

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,12 +12,14 @@ def state_list():
     states = storage.all("State").values()
     states = sorted(states, key=lambda state: state.name)
     return render_template("7-states_list.html",
-                            states=states)
+                           states=states)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """request to remove the current SQLAlchemy Session"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
