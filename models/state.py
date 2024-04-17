27ca1524803ebd8f0294702ea0d5c29a478cce5c
ade-mic <ademic.aina@gmail.com>
2024-04-17 10:21:33 +0100

    db_storage

--- models/state.py
+++ models/state.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
 """ State Class """
-import models
-from models.base_model import BaseModel, Base
-from models.city import City
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
 
 class State(BaseModel, Base):
     """Representation of state """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
     if models.storage_t == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
     else:
-        name = ""
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            return [city for city in models.storage.all(City).values() if city.state_id == self.id]
 
     def __init__(self, *args, **kwargs):
         """initializes state"""
         super().__init__(*args, **kwargs)
-
-    if models.storage_t != "db":
-        @property
-        def cities(self):
-            """getter for list of city instances related to the state"""
-            city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
